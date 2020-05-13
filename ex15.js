31fc37fa9c7e2e8364b360db266db1c6c885f678
EdgarCortez1 <ea.cortezz@gmail.com>
2020-05-12 19:27:15 -0500

    added exercise 15

---
+++ ex15.js
@@ -0,0 +1,35 @@
+// Exercise 15: If and Else
+
+let x = 10;
+
+if (x === 10){
+    // First jump
+    console.log("x is equal to 10: ", x);
+} else if (x < 10){
+    // Second jump
+    console.log("x is less than 10: ", x);
+} else {
+    // Last jump
+    console.log("x is greater than 10: ", x);
+}
+
+/** Consider this part a puzzle to solve
+ *  what's the least number of times you have to change
+ *  the code to make each console.log run?
+ */
+
+ let y = 100;
+
+ if (y === 100) {
+     if (x < 10) {
+         console.log('log 1', x, y);
+     } else {
+         console.log('log 2', x, y);
+     }
+ } else {
+     if (x > 10) {
+         console.log("log 3", x, y);
+     } else {
+         console.log("log 4", x, y);
+     }
+ }
